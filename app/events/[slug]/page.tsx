255e05ebd9e09c84d704d59c3de26bba7910cca9
Alan-Alkalifa <work.alanalkalifa@gmail.com>
2024-12-09 12:30:00 +0700

    Updated

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -10,10 +10,10 @@ import FadeIn from '../../../components/animations/FadeIn';
 const getEventBySlug = (slug: string) => {
     const events = {
         'sustainable-workshop': {
-            title: "Sustainability Workshop",
-            date: "2024-12-15",
-            time: "01:00 PM",
-            location: "Gedung A Univeritas Pembangunan Jaya",
+            title: "Clean4Earth Sustainability Workshop",
+            date: "2024-12-12",
+            time: "10:00 AM - 12:00 AM",
+            location: "Gedung B Lantai 1 - B104",
             description: "Learn practical tips for sustainable living.",
             image: "https://images.unsplash.com/photo-1544928147-79a2dbc1f389?ixlib=rb-4.0.3"
         },
@@ -26,12 +26,20 @@ const getEventBySlug = (slug: string) => {
             image: "https://images.unsplash.com/photo-1563986768609-322da13575f3?ixlib=rb-4.0.3"
         },
         'open-booth-campaign': {
-            title: "Open Booth Campaign",
+            title: "Exhibition Clean4Earth",
             date: "2024-12-23",
             time: "07:00 AM - 03.00 PM",
-            location: "Aula lt3 Gedung B UPJ",
+            location: "Aula Lantai 3 Gedung B UPJ",
             description: "Come and learn about our sustainability initiatives.",
             image: "https://images.unsplash.com/photo-1523240795612-9a054b0db644?ixlib=rb-4.0.3"
+        },
+        'clean4earth-introducer': {
+            title: "Clean4Earth Introducer",
+            date: "2024-11-23",
+            time: "09:00 AM",
+            location: "Online",
+            description: "Online Information to introduce Clean4Earth.",
+            image: "https://images.unsplash.com/photo-1560523159-6b681a1e1852?ixlib=rb-4.0.3"
         }
     };
     return events[slug as keyof typeof events];
